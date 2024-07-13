9c914b869300eaab7fcd89c2c2bf26bad4b3fed3
Vivek Bhavsar <vivek131299@gmail.com>
2024-07-13 16:25:21 +0530

    first commit - project setup, added models

---
+++ api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class ProductStatus(models.TextChoices):
+    UPLOADED = "UPLOADED"
+    REJECTED = "REJECTED"
+    SUCCESS = "SUCCESS"
+    CANCELLED = "CANCELLED"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    category_id = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    price = models.FloatField(default=0)
+    status = models.CharField(max_length=50, choices=ProductStatus.choices, default=ProductStatus.UPLOADED)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
